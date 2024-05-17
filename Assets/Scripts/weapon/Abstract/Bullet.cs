c20b499ce22f75dc7c7da6fe8efdd5cad38034d6
E-Dawnize <157566656+E-Dawnize@users.noreply.github.com>
2024-05-17 22:23:47 +0800

    添加子弹追踪实现

--- Assets/Scripts/weapon/Abstract/Bullet.cs
+++ Assets/Scripts/weapon/Abstract/Bullet.cs
@@ -8,6 +8,12 @@ public abstract class Bullet : MonoBehaviour
     public Vector3 Direction;//速度方向，模为一
     public GameObject TargetEnemy;//追踪弹目标
     public abstract void GetInitialVelocity();//给予初速度
-    public abstract void GetTargetEnemy();//给予追踪目标
+
+    //追踪函数，放到lateUpdate
+    public virtual void TraceTargetEnemy(){
+        Direction=(TargetEnemy.transform.position-gameObject.transform.position).normalized;
+        float AngleOfZ= Vector3.Cross(Direction,new Vector3(0,0,transform.position.z)).z>0?Vector3.Angle(Direction,new Vector3(0,0,transform.position.z)):-Vector3.Angle(Direction,new Vector3(0,0,transform.position.z));;
+        transform.rotation=Quaternion.Lerp(transform.rotation,Quaternion.Euler(0,0,AngleOfZ),2f);
+    }   
     protected abstract void OnCollisionEnter2D(Collision2D other);//碰撞（击中）检测
 }
