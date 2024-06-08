b4c24766932a55569329c14497ba11f98c7da7b1
EVE1012 <2638212425@qq.com>
2024-06-08 22:33:14 +0800

    地图初步（教程03）
    创建房间的门
    创建房间的代码 Room 设置所需要的参数
    设置最后房间的思路
    优化找房间的方法

---
+++ Assets/Scripts/Map/RoomGenerator.cs
@@ -0,0 +1,145 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RoomGenerator : MonoBehaviour
+{
+    public enum Direction { up, down, left, right };
+    public Direction direction;
+
+    [Header("房间信息")]
+    public GameObject roomPrefab;
+    public int roomNumber;
+    public Color startColor, endColor;
+    private GameObject endRoom;
+
+    [Header("位置控制")]
+    public Transform generatorPoint;
+    public float xOffset;
+    public float yOffset;
+    public LayerMask roomLayer;
+    public int maxStep;
+
+    public List<Room> rooms = new List<Room>();
+
+    List<GameObject> farRooms = new List<GameObject>();
+    List<GameObject> lessFarRooms = new List<GameObject>();
+
+    List<GameObject> oneWayRooms = new List<GameObject>();
+
+    void Start()
+    {
+        for (int i = 0; i < roomNumber; i++)
+        {
+            rooms.Add(Instantiate(roomPrefab, generatorPoint.position, Quaternion.identity).GetComponent<Room>());
+
+            //改变point位置
+            ChangePointPos();
+        }
+
+        rooms[0].GetComponent<SpriteRenderer>().color = startColor;
+
+
+        endRoom = rooms[0].gameObject;
+
+        //找到最后房间
+        foreach (var room in rooms)
+        {
+            // if (room.transform.position.sqrMagnitude > endRoom.transform.position.sqrMagnitude)
+            // {
+            //    endRoom = room.gameObject;
+            // }
+
+            SetupRoom(room, room.transform.position);
+        }
+
+        FindEndRoom();
+
+        endRoom.GetComponent<SpriteRenderer>().color = endColor;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.anyKeyDown)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+
+    public void ChangePointPos()
+    {
+        do
+        {
+            direction = (Direction)Random.Range(0, 4);
+
+            switch (direction)
+            {
+                case Direction.up:
+                    generatorPoint.position += new Vector3(0, yOffset, 0);
+                    break;
+                case Direction.down:
+                    generatorPoint.position += new Vector3(0, -yOffset, 0);
+                    break;
+                case Direction.left:
+                    generatorPoint.position += new Vector3(-xOffset, 0, 0);
+                    break;
+                case Direction.right:
+                    generatorPoint.position += new Vector3(xOffset, 0, 0);
+                    break;
+            }
+        } while (Physics2D.OverlapCircle(generatorPoint.position, 0.2f, roomLayer));
+    }
+
+    public void SetupRoom(Room newRoom, Vector3 roomPosition)
+    {
+        newRoom.roomUp = Physics2D.OverlapCircle(roomPosition + new Vector3(0, yOffset, 0), 0.2f, roomLayer);
+        newRoom.roomDown = Physics2D.OverlapCircle(roomPosition + new Vector3(0, -yOffset, 0), 0.2f, roomLayer);
+        newRoom.roomLeft = Physics2D.OverlapCircle(roomPosition + new Vector3(-xOffset, 0, 0), 0.2f, roomLayer);
+        newRoom.roomRight = Physics2D.OverlapCircle(roomPosition + new Vector3(xOffset, 0, 0), 0.2f, roomLayer);
+
+        newRoom.UpdateRoom();
+    }
+
+    public void FindEndRoom()
+    {
+        //最大数值
+        for (int i = 0; i < rooms.Count; i++)
+        {
+            if (rooms[i].stepToStart > maxStep)
+                maxStep = rooms[i].stepToStart;
+        }
+
+        //获得最大值房间和次大值
+        foreach(var room in rooms)
+        {
+            if (room.stepToStart == maxStep)
+                farRooms.Add(room.gameObject);
+            if (room.stepToStart == maxStep - 1)
+                lessFarRooms.Add(room.gameObject);
+        }
+
+        for (int i = 0; i < farRooms.Count; i++)
+        {
+            if (farRooms[i].GetComponent<Room>().doorNumber == 1)
+                oneWayRooms.Add(farRooms[i]);
+        }
+        
+        for (int i = 0; i < lessFarRooms.Count; i++)
+        {
+            if (lessFarRooms[i].GetComponent<Room>().doorNumber == 1)
+                oneWayRooms.Add(lessFarRooms[i]);
+        }
+
+        if(oneWayRooms.Count != 0)
+        {
+            endRoom = oneWayRooms[Random.Range(0, oneWayRooms.Count)];
+        }
+        else
+        {
+            endRoom = farRooms[Random.Range(0, farRooms.Count)];    
+        }
+    }
+}
