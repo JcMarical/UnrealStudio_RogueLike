cc38ca325cb1fd8dce7863622c7c5761dbdfb57c
EVE1012 <2638212425@qq.com>
2024-06-10 22:26:54 +0800

    地图初步（教程06）
    摄像机移动代码
    房间判断人物进屋代码

--- Assets/Scripts/Map/Room.cs
+++ Assets/Scripts/Map/Room.cs
@@ -38,5 +38,11 @@ public class Room : MonoBehaviour
             doorNumber++;
     }
 
-
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            CameraController.instance.ChangeTarget(transform);
+        }
+    }
 }
