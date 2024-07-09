4117a08798e3a411a84473ccfd7b11790f9bf04d
EVE1012 <2638212425@qq.com>
2024-07-09 13:00:54 +0800

    地图完善
    更新tab键召唤小地图

---
+++ Assets/Scripts/Map/MapImage.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class MapImage : MonoBehaviour
+{
+    public GameObject m_Sprite;
+   
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            m_Sprite.SetActive(true);
+        }
+        if (Input.GetKeyUp(KeyCode.Tab))
+        {
+            m_Sprite.SetActive(false);
+        }
+    }
+}
