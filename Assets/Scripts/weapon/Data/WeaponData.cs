b0196f521a5f0c773a85dc2af079fd414f43b4c1
E-Dawnize <157566656+E-Dawnize@users.noreply.github.com>
2024-07-10 15:52:44 +0800

    添加实际值与基础值区分

--- Assets/Scripts/weapon/Data/WeaponData.cs
+++ Assets/Scripts/weapon/Data/WeaponData.cs
@@ -5,20 +5,34 @@ using UnityEngine;
 [Serializable]
 public struct WeaponData
 {
-    //武器数据结构体，_bas为初始值
+    //武器数据结构体，_bas为初始值，_fac为实际值
+    #region 属性
         public int rarity;//稀有度
         public int id;//武器编号
-        public float DamageValue_bas;//伤害
-        public float AttachRadius_bas;//攻击半径
-        public float AttachInterval_bas;//攻击间隔
         public DamageKind damageKind;//伤害类型
-        public Weapon_EffectType specialEffect;//特殊效果
-        public float RepelAbility_bas;//击退能力
-        public float MaxPower_bas;//满能量数值
-        public float CurrentPower;//当前能量
-        public float DefaultCharge_Value;//默认单次攻击充能量
-        public float Weight_bas;//武器重量
+        public Weapon_EffectType specialEffect;//特殊效果 
         public Sprite sprite;//武器图片
         public int segment;//攻击段数
         public List<Collider2D> Range;//武器不同段攻击的不同碰撞箱
+        #endregion
+
+        #region 基础数值
+        public float DamageValue_bas;//伤害
+        public float AttachRadius_bas;//攻击半径
+        public float AttachInterval_bas;//攻击间隔
+        public float RepelAbility_bas;//击退能力
+        public float MaxPower_bas;//满能量数值
+        public float Weight_bas;//武器重量
+        #endregion
+
+        #region 实际数值
+        public float DamageValue_fac;//伤害
+        public float AttachRadius_fac;//攻击半径
+        public float AttachInterval_fac;//攻击间隔
+        public float RepelAbility_fac;//击退能力
+        public float Power_fac;//满能量数值
+        public float DefaultCharge_Value;//默认单次攻击充能量
+        public float Weight_fac;//武器重量
+       #endregion
+        
 }
