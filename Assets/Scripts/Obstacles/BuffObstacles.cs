4d8ea89fe6a77018e3a75d95db50ea0db95d4d60
pypsada <3223175866@qq.com>
2024-08-10 20:26:50 +0800

    buff地面

--- Assets/Scripts/Obstacles/BuffObstacles.cs
+++ Assets/Scripts/Obstacles/BuffObstacles.cs
@@ -1,10 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Reflection.Emit;
 using UnityEngine;
 
 public class BuffObstacles : MonoBehaviour
 {
     public PlayerSS_FSM playerSSFSM;
+    public string buff;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,7 +23,7 @@ public class BuffObstacles : MonoBehaviour
     {
         if (playerSSFSM != null && collision.CompareTag("player"))
         {
-            playerSSFSM.AddState(new SS_Sticky(), 10);
+            playerSSFSM.AddState(buff, 2);
         }
     }
 }
