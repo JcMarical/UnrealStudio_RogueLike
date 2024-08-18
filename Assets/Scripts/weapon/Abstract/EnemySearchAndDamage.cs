a6d2af9e65540fe040f4d48f5c8685450614d7e9
NSHYuki <3109209420@qq.com>
2024-08-18 21:43:34 +0800

    小改一个变量名

--- Assets/Scripts/weapon/Abstract/EnemySearchAndDamage.cs
+++ Assets/Scripts/weapon/Abstract/EnemySearchAndDamage.cs
@@ -109,7 +109,7 @@ public class EnemySearchAndDamage : MonoBehaviour
     /// <param name="enemy"></param>
     /// <returns>伤害和倍率是否大于2</returns>
    public (float,bool) getWeaponDirectHitValue(Enemy enemy){
-        float Mutiple=(1+PlayerBuffMonitor.Instance.InjuryBuff)* (1 / (Mathf.Log(2, enemy.rb.mass) + 1)) * enemy.getHitMultiple;
+        float Mutiple=(1+PlayerBuffMonitor.Instance.InjuryBuff)* (1 / (Mathf.Log(2, enemy.rb.mass) + 1)) * enemy.getDamageMultiple;
         return ((Player.Instance.playerData.playerAttack+UnityEngine.Random.Range(-0.2f,0.2f))*WeaponCtrl.Instance.GetWeaponData()[0].DamageValue_bas*Mutiple,
         Mutiple>=2?true:false);
     }
