2df48424b6565d763758ba24dedcbe4dff1823d4
EVE1012 <2638212425@qq.com>
2024-08-18 15:06:20 +0800

    l地牢生成
    利用随机步法生成的地牢（仍存在缺陷）

--- Assets/Scripts/Map/RoomGenerator.cs
+++ Assets/Scripts/Map/RoomGenerator.cs
@@ -10,11 +10,6 @@ public class RoomGenerator : MonoBehaviour
 
     [Header("房间信息")]
     public GameObject roomPrefab;
-    public GameObject roomPrefab32_1; // 新的长房间
-    public GameObject roomPrefab16_2; // 新的宽房间
-
-    public int roomNumber;
-    public Color startColor, endColor;
     private GameObject endRoom;
 
     [Header("位置控制")]
@@ -23,6 +18,7 @@ public class RoomGenerator : MonoBehaviour
     public float yOffset;
     public LayerMask roomLayer;
     public int maxStep;
+    public int maxRoomCount;
 
     public List<Room> rooms = new List<Room>();
 
@@ -32,142 +28,98 @@ public class RoomGenerator : MonoBehaviour
 
     List<GameObject> oneWayRooms = new List<GameObject>();
 
-    private bool IsPreviousRoomSpecial = false;
-    private GameObject lastRoomType = null; // 用于存储上一个房间的类型
     public WallType wallType;
+
+    private Vector3[] directions;
 
     void Start()
     {
-        GenerafateRoom();
+        directions = new Vector3[]
+       {
+            new Vector3(0, yOffset, 0),   // up
+            new Vector3(0, -yOffset, 0),  // down
+            new Vector3(-xOffset, 0, 0),  // left
+            new Vector3(xOffset, 0, 0)    // right
+       };
+
+        StartCoroutine(GenerateRoomsCoroutine());
     }
 
-    void GenerafateRoom()
+    IEnumerator GenerateRoomsCoroutine()
     {
-        for (int i = 0; i < roomNumber; i++)
+        List<Vector3> currentPositions = new List<Vector3> { generatorPoint.position };  // 初始位置
+        List<Vector3> nextPositions = new List<Vector3>();
+
+        bool stopGeneration = false;
+        while (!stopGeneration && rooms.Count < maxRoomCount)
         {
-            GameObject roomToInstantiate;
-
-            if (i == 0 || IsPreviousRoomSpecial == true)   //第一个房间/之前的房间是特殊房间：下一个房间正常
+            for (int i = 0; i < currentPositions.Count; i++)
             {
-                roomToInstantiate = roomPrefab;
+                Vector3 currentPos = currentPositions[i];
+
+                // 随机选择一个方向
+                List<int> availableDirections = new List<int> { 0, 1, 2, 3 };
+                while (availableDirections.Count > 0)
+                {
+                    int randomIndex = Random.Range(0, availableDirections.Count);
+                    int directionIndex = availableDirections[randomIndex];
+                    availableDirections.RemoveAt(randomIndex);
+
+                    Vector3 newPos = currentPos + directions[directionIndex];
+
+                    if (!Physics2D.OverlapCircle(newPos, 0.2f, roomLayer) && IsValidPosition(newPos))
+                    {
+                        rooms.Add(Instantiate(roomPrefab, newPos, Quaternion.identity).GetComponent<Room>());
+                        nextPositions.Add(newPos);
+
+                        // 检查是否超出大正方形
+                        if (IsOutOfBoundary(newPos))
+                        {
+                            stopGeneration = true;
+                            break;
+                        }
+
+                        // 每次只在一个方向生成一个房间
+                        break;
+                    }
+                }
+
+                if (stopGeneration)
+                    break;
             }
-            else    // 如果上一个房间是正常房间，下一个任选
+
+            currentPositions = new List<Vector3>(nextPositions);
+            nextPositions.Clear();
+
+            yield return null;
+
+            foreach (var room in rooms)
             {
-                roomToInstantiate = GetRandomRoomPrefab();
+                SetupRoom(room, room.transform.position);
             }
-
-
-            rooms.Add(Instantiate(roomToInstantiate, generatorPoint.position, Quaternion.identity).GetComponent<Room>());   //对象，位置，方向，获取其Room组件并添加到room之中
-
-            IsPreviousRoomSpecial = roomToInstantiate == roomPrefab32_1 || roomToInstantiate == roomPrefab16_2;
-
-            lastRoomType = roomToInstantiate;
-
-            ChangePointPos(roomToInstantiate);
         }
-
-
-
-
-
-
-        endRoom = rooms[0].gameObject;
-
-        //找到最后房间
-        foreach (var room in rooms)
-        {
-            // if (room.transform.position.sqrMagnitude > endRoom.transform.position.sqrMagnitude)
-            // {
-            //    endRoom = room.gameObject;
-            // }
-
-            //SetupRoom(room, room.transform.position);
-        }
-
-        FindEndRoom();
-
-        endRoom.GetComponent<SpriteRenderer>().color = endColor;
-
     }
-
-
-    public void ChangePointPos(GameObject roomPrefab)
+    private bool IsOutOfBoundary(Vector3 position)
     {
-        Vector3 currentPos = generatorPoint.position;
-
-        float roomWidth = xOffset;
-        float roomHeight = yOffset;
-
-        // 根据下一个房间的类型调整偏移量
-        if (!IsPreviousRoomSpecial)
-        {
-            if (roomPrefab == roomPrefab32_1)
-            {
-                roomWidth *= 1.5f; // 下一个房间是3.2:1，调整宽度
-            }
-            else if (roomPrefab == roomPrefab16_2)
-            {
-                roomHeight *= 1.5f; // 下一个房间是1.6:2，调整高度
-            }
-        }
-        else
-        {
-            // 上一个房间是特殊房间，调整为普通房间
-            if (lastRoomType == roomPrefab32_1)
-            {
-                roomWidth *= 1.5f; // 当前房间是3.2:1，调整宽度
-            }
-            else if (lastRoomType == roomPrefab16_2)
-            {
-                roomHeight *= 1.5f; // 当前房间是1.6:2，调整高度
-            }
-        }
-
-
-        do
-        {
-            direction = (Direction)Random.Range(0, 4);
-
-            switch (direction)
-            {
-                case Direction.up:
-                    generatorPoint.position += new Vector3(0, roomHeight, 0);
-                    break;
-                case Direction.down:
-                    generatorPoint.position += new Vector3(0, -roomHeight, 0);
-                    break;
-                case Direction.left:
-                    generatorPoint.position += new Vector3(-roomWidth, 0, 0);
-                    break;
-                case Direction.right:
-                    generatorPoint.position += new Vector3(roomWidth, 0, 0);
-                    break;
-            }
-        } while (Physics2D.OverlapCircle(generatorPoint.position, 0.2f, roomLayer)) ;
-
+        float halfSize = (maxRoomCount / 2) * xOffset;  // 大正方形的一半长度
+        return Mathf.Abs(position.x) > halfSize || Mathf.Abs(position.y) > halfSize;
     }
-    
-    GameObject GetRandomRoomPrefab()    //按照随机数生成不同类型的房间,返回房间的预制体
+
+    private bool CheckStopCondition()
     {
-        int rand = Random.Range(0, 3);
-        if (rand == 0) return roomPrefab;
-        else if (rand == 1) return roomPrefab32_1;
-        else return roomPrefab16_2;
+        int maxRooms = 24; // 可以根据复杂性或其他条件动态计算
+        return rooms.Count >= maxRooms;
     }
 
-    /*public void SetupRoom(Room newRoom, Vector3 roomPosition)
+    private bool IsValidPosition(Vector3 position)
+    {
+        return !Physics2D.OverlapCircle(position, 0.2f, roomLayer);
+    }
+
+    public void SetupRoom(Room newRoom, Vector3 roomPosition)
     {
         float roomWidth = xOffset;
         float roomHeight = yOffset;
-
-        if (newRoom.gameObject == roomPrefab32_1)
-        {
-            roomWidth *= 2;
-        }
-        else if (newRoom.gameObject == roomPrefab16_2)
-        {
-            roomHeight *= 2;
-        }
 
         newRoom.roomUp = Physics2D.OverlapCircle(roomPosition + new Vector3(0, roomHeight, 0), 0.2f, roomLayer);
         newRoom.roomDown = Physics2D.OverlapCircle(roomPosition + new Vector3(0, -roomHeight, 0), 0.2f, roomLayer);
@@ -217,7 +169,7 @@ public class RoomGenerator : MonoBehaviour
                     Instantiate(wallType.fourDoors, roomPosition, Quaternion.identity);
                 break;
         }
-    }*/
+    }
 
     public void FindEndRoom()
     {
@@ -229,7 +181,7 @@ public class RoomGenerator : MonoBehaviour
         }
 
         //获得最大值房间和次大值
-        foreach(var room in rooms)
+        foreach (var room in rooms)
         {
             if (room.stepToStart == maxStep)
                 farRooms.Add(room.gameObject);
@@ -242,23 +194,25 @@ public class RoomGenerator : MonoBehaviour
             if (farRooms[i].GetComponent<Room>().doorNumber == 1)
                 oneWayRooms.Add(farRooms[i]);
         }
-        
+
         for (int i = 0; i < lessFarRooms.Count; i++)
         {
             if (lessFarRooms[i].GetComponent<Room>().doorNumber == 1)
                 oneWayRooms.Add(lessFarRooms[i]);
         }
 
-        if(oneWayRooms.Count != 0)
+        if (oneWayRooms.Count != 0)
         {
             endRoom = oneWayRooms[Random.Range(0, oneWayRooms.Count)];
         }
         else
         {
-            endRoom = farRooms[Random.Range(0, farRooms.Count)];    
+            endRoom = farRooms[Random.Range(0, farRooms.Count)];
         }
     }
 }
+
+
 
 
 [System.Serializable]
