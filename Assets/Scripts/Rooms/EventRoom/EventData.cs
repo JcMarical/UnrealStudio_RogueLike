af422e88bb9a716d47793b32da130cb1778df399
NSHYuki <3109209420@qq.com>
2024-08-25 23:24:56 +0800

    继续事件房框架

--- Assets/Scripts/Rooms/EventRoom/EventData.cs
+++ Assets/Scripts/Rooms/EventRoom/EventData.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-[CreateAssetMenu(fileName = "EventData", menuName = "Data/Rooms/EventRoom", order = 1)]
 public class EventData : ScriptableObject
 {
     [Serializable]
@@ -12,6 +11,7 @@ public class EventData : ScriptableObject
     {
         [Tooltip("选项名称")] public string title;
         [Tooltip("选项描述")][TextArea(2, 4)] public string description;
+        [Tooltip("选项结果")][TextArea(2, 4)] public string result;
     }
 
     [Tooltip("背景图片")] public Image backgroundImage;
@@ -19,4 +19,28 @@ public class EventData : ScriptableObject
     [Tooltip("事件描述")][TextArea(3, 6)] public string eventDescription;
     [Tooltip("选项")] public Choice[] choices;
     [Tooltip("事件能否重复出现")] public bool isRepeatable;
+
+    public virtual void Choose0() => EventRoomMgr.Instance.choiceNumber = 0;
+    public virtual void Choose1() => EventRoomMgr.Instance.choiceNumber = 1;
+    public virtual void Choose2() => EventRoomMgr.Instance.choiceNumber = 2;
+    public virtual void Choose3() => EventRoomMgr.Instance.choiceNumber = 3;
+}
+
+[CreateAssetMenu(fileName = "WeirdDice", menuName = "Data/Events/WeirdDice", order = 0)]
+public class WeirdDiceEvent : EventData
+{
+    public override void Choose0()
+    {
+        base.Choose0();
+    }
+
+    public override void Choose1()
+    {
+        base.Choose1();
+    }
+
+    public override void Choose2()
+    {
+        base.Choose2();
+    }
 }
