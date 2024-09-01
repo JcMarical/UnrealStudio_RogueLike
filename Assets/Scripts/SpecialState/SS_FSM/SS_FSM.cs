bb8ecde39c7ffe25858580a9d11f67590b3591aa
YingMobit <151351945+YingMobit@users.noreply.github.com>
2024-09-01 19:22:48 +0800

    有地方忘记激活事件了

--- Assets/Scripts/SpecialState/SS_FSM/SS_FSM.cs
+++ Assets/Scripts/SpecialState/SS_FSM/SS_FSM.cs
@@ -75,6 +75,7 @@ public class SS_FSM : MonoBehaviour
                 foreach (SpecialState SubState in Subordinates)
                 {
                     SubState.StateExit(StatesList);
+                    WhenStateExit?.Invoke();
                 }
             }
         }
