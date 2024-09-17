2f0d8ba49383e666dbea99762e62a63c7ced2f63
E-Dawnize <157566656+E-Dawnize@users.noreply.github.com>
2024-09-17 18:24:26 +0800

    拾取ui完事
    ui真是好活哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈（死了

--- Assets/Scripts/UI&Events/DragImage.cs
+++ Assets/Scripts/UI&Events/DragImage.cs
@@ -7,6 +7,7 @@ using UnityEngine.UI;
 public class DragImage : MonoBehaviour
 {
     public int index;
+    [SerializeField]
     private GameObject weapon;
     public bool PickWeapon_IsNull=true;
     public GameObject Weapon{
@@ -14,6 +15,7 @@ public class DragImage : MonoBehaviour
         set { 
             weapon = value; 
             if(value != null){
+                IsInteractable=true;
                 image.gameObject.SetActive(true);
                 image.sprite=weapon.GetComponent<Weapon>().weaponData.sprite;
             }
@@ -23,6 +25,7 @@ public class DragImage : MonoBehaviour
             }
         }
     }
+    [SerializeField]
     bool isInteractable=true;
     public bool IsInteractable{
         get{return isInteractable;}
@@ -37,19 +40,25 @@ public class DragImage : MonoBehaviour
         }
     }
     public Image image=>transform.GetChild(0).GetChild(0).GetComponent<Image>();
-    public Vector3 prePos;
+    public Vector3 prePos=>image.transform.parent.position;
     Action update;
     bool isDrag;
-    private void Start() =>prePos=image.transform.position;
+    // private void Start() =>prePos=image.transform.position;
     void Update()
     {
         update?.Invoke();
         if(Input.GetMouseButtonDown(0)&&isMouseIn()&&isInteractable&&PickWeapon_IsNull){
             PickWeaponPanel.Instance.dragingImage=this;
             isDrag=true;
-            prePos=image.transform.position;
+            //prePos=image.transform.position;
             update+=()=>{
-                image.transform.position=Input.mousePosition;
+                Vector2 mousePos;  
+                RectTransformUtility.ScreenPointToLocalPointInRectangle(  
+                    image.transform.parent.GetComponent<RectTransform>(),  
+                    Input.mousePosition,  
+                    Camera.main,  
+                    out mousePos);   
+                image.rectTransform.anchoredPosition = mousePos;  
             };
         }
         else if(Input.GetMouseButtonUp(0)&&PickWeapon_IsNull){
@@ -66,7 +75,7 @@ public class DragImage : MonoBehaviour
 
         // 将屏幕坐标转换为 UI 元素的本地坐标
         Vector2 localPoint;
-        RectTransformUtility.ScreenPointToLocalPointInRectangle(GetComponent<RectTransform>(), mousePosition, null, out localPoint);
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(GetComponent<RectTransform>(), mousePosition, Camera.main, out localPoint);
 
         // 判断鼠标是否在 UI 元素的 RectTransform 内部
         return GetComponent<RectTransform>().rect.Contains(localPoint);
