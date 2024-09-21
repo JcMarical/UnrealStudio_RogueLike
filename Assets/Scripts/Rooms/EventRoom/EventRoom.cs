31a6cd4c05488127741b00da68020989536b13d9
Yuki Hikariyo <3109209420@qq.com>
2024-09-21 19:22:41 +0800

    事件房敌人生成需适应新房间

--- Assets/Scripts/Rooms/EventRoom/EventRoom.cs
+++ Assets/Scripts/Rooms/EventRoom/EventRoom.cs
@@ -4,41 +4,11 @@ using UnityEngine;
 
 public class EventRoom : MonoBehaviour
 {
-    public Transform zeroPosition;
     public Transform centerPosition;
-    public Vector3[,] positions;
-    public List<Vector3> validPositionsList;
 
-    public Vector2 playerDetectSize = new Vector2(14, 8);
+    public Vector2 playerDetectSize = new Vector2(24, 14);
     public LayerMask playerLayer = 1 << 6;
     public bool isHappened;
-
-    private void Awake()
-    {
-        positions = new Vector3[14, 8];
-
-        for (int i = 0; i < 14; i++)
-        {
-            for (int j = 0; j < 8; j++)
-            {
-                positions[i, j] = zeroPosition.position + new Vector3(i, j, 0);
-                validPositionsList.Add(positions[i, j]);
-            }
-        }
-
-        validPositionsList.Remove(positions[1, 1]);
-        validPositionsList.Remove(positions[1, 2]);
-        validPositionsList.Remove(positions[2, 1]);
-        validPositionsList.Remove(positions[12, 1]);
-        validPositionsList.Remove(positions[12, 2]);
-        validPositionsList.Remove(positions[11, 1]);
-        validPositionsList.Remove(positions[1, 6]);
-        validPositionsList.Remove(positions[1, 5]);
-        validPositionsList.Remove(positions[2, 6]);
-        validPositionsList.Remove(positions[12, 6]);
-        validPositionsList.Remove(positions[12, 5]);
-        validPositionsList.Remove(positions[11, 6]);
-    }
 
     private void Update()
     {
